b71cff02567440f359036ce17e3f2b04b5873efe
alican sungur <sunguralican@gmail.com>
2019-06-30 18:39:57 +0000

    new mortgage calculator

---
+++ L2/mortgage_calculator.rb
@@ -0,0 +1,22 @@
+# prompt message
+
+# Welcome message
+
+# Ask for the loan amount (give example 200000 for 200,000$)
+
+# Check formatting of the loan amount, if invalid loop it back to question
+
+# Ask for the interested rate (for instance 5 for 5%)
+
+# Check if the formatting correct otherwise loop back to the question
+
+# Ask for the loan duration (for instance 16 for 16 months/1.5 years)
+
+# Check if the number is integer and positive otherwise go back to question
+
+# Calculate the payment and give the message
+
+# Ask if user wants to run another calculation
+
+# If not say thanks and bye
+
