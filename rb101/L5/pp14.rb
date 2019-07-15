35eae26ee620065f8a7f63886c42a7f73e511d65
asungur <sunguralican@gmail.com>
2019-07-15 19:34:47 +0000

    L5 completed

---
+++ rb101/L5/pp14.rb
@@ -0,0 +1,23 @@
+=begin
+Given this data structure write some code to 
+return an array containing the colors of the fruits 
+and the sizes of the vegetables.
+The sizes should be uppercase and the colors should be capitalized.
+=end
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+hsh.map do |key, value|
+  if value[:type] == 'fruit'
+    value[:colors].map do |color|
+      color.capitalize
+    end
+  elsif value[:type] == 'vegetable'
+    value[:size].upcase
+  end
+end
