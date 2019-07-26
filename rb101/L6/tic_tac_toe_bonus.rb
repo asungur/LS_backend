4ca8cf0c1296705d6202d05b64a4b830c92a42f8
asungur <sunguralican@gmail.com>
2019-07-26 16:47:55 +0000

    simplified

--- rb101/L6/tic_tac_toe_bonus.rb
+++ rb101/L6/tic_tac_toe_bonus.rb
@@ -1,16 +1,3 @@
-require 'pry'
-=begin
-1. Display the initial empty 3x3 board
-2. Ask the user to mark a square
-3. Computer marks a square.
-4. Display the updated board state
-5. If winner, display winner.
-6. If board full, display tie.
-7. Nor go to #2
-8. Play again?
-9. If yes, go to #1
-10.Ending message
-=end
 FIRST_MOVE = 'player'
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
