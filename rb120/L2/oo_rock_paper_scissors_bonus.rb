7a35092030ef61d1845546c9f091db1fae20fe4e
asungur <sunguralican@gmail.com>
2019-08-24 09:33:20 +0000

    OO RPSLS for review

--- rb120/L2/oo_rock_paper_scissors_bonus.rb
+++ rb120/L2/oo_rock_paper_scissors_bonus.rb
@@ -6,7 +6,7 @@ class Move
              spock: %w(sp spock) }.freeze
 
   def initialize(val)
-    @value = val.downcase.to_s
+    @value = val.to_s.downcase
   end
 
   # IS? METHODS
@@ -17,7 +17,7 @@ class Move
   def paper?
     @value == 'paper' || @value == 'p'
   end
-  
+
   def scissors?
     @value == 'scissors' || @value == 's'
   end
@@ -25,53 +25,54 @@ class Move
   def lizard?
     @value == 'lizard' || @value == 'l'
   end
-  
+
   def spock?
     @value == 'spock' || @value == 'sp'
   end
-  
+
   # LOSE TO METHODS
   def lose_to_rock?
     lizard? || scissors?
   end
-  
+
   def lose_to_paper?
     rock? || spock?
   end
-  
+
   def lose_to_scissors?
     paper? || lizard?
   end
-  
+
   def lose_to_lizard?
     spock? || paper?
   end
-  
+
   def lose_to_spock?
     rock? || scissors?
   end
-  
+
   # WIN TO METHODS
   def win_to_rock?
     spock? || paper?
   end
-  
+
   def win_to_paper?
     lizard? || scissors?
   end
-  
+
   def win_to_scissors?
     rock? || spock?
   end
-  
+
   def win_to_lizard?
     rock? || scissors?
   end
-  
+
   def win_to_spock?
     paper? || lizard?
   end
 
+  # rubocop:disable Metrics/PerceivedComplexity , Metrics/CyclomaticComplexity
   def >(other_move)
     (rock? && other_move.lose_to_rock?) ||
       (paper? && other_move.lose_to_paper?) ||
@@ -87,7 +88,8 @@ class Move
       (lizard? && other_move.win_to_lizard?) ||
       (spock? && other_move.win_to_spock?)
   end
-  
+  # rubocop:enable Metrics/PerceivedComplexity , Metrics/CyclomaticComplexity
+
   def to_s
     @value
   end
@@ -101,20 +103,19 @@ class Player
     set_name
     @score = 0
   end
-  
+
   # SCORE RELATED METHODS
   def update_score
     self.score += 1
   end
-  
+
   def reset_score
     self.score = 0
   end
-  
+
   private
-  
+
   attr_writer :move_history
-  
 end
 
 class Human < Player
@@ -128,11 +129,11 @@ class Human < Player
     end
     self.name = n
   end
-  
+
   def valid_choice?(val)
     Move::VALUES.values.flatten.include? val
   end
-  
+
   def valid_choice(val)
     Move::VALUES.each_pair do |k, v|
       return k if v.include? val
@@ -152,37 +153,84 @@ class Human < Player
 end
 
 class Computer < Player
+  attr_accessor :bhvr
   # COMPUTER BEHAVIOUR
-  # Behaviour 1 AKA Counter => 
+  # Behaviour 1 AKA Counter =>
   #   1. Analyses player history
   #   2. Find the most common choice
   #   3. Find two counterpicks
   #   4. Put them in an array with another losing pick
   #   5. Choose a random pick from this list
   #   6. %66 percent counter pick to most common player choice
-  
+
   # Behaviour 2 AKA Random
   #   Choose randomly
-  
+
   # Behaviour 3 AKA Dumb
   #   Chooses rock only
-  
+
   # Behaviour 4 AKA Old-man
   #   Chooses the 3 options from the prev. version of the game
   # -------------------------
-  
-  COMPUTER_SKILL_ASSG = { 'R2D2' => 1, 'Hal' =>1,
-                          'Chappie' => 4, 'Sonny' => 2,
-                          'Number 5' => 3}.freeze
 
-  def set_computer
+  COMPUTER_SKILL_ASSG = { 'R2D2' => 1, 'Hal' => 1,
+                          'Chappie' => 4, 'Sonny' => 2,
+                          'Number 5' => 3 }.freeze
+
+  def set_name
     i = rand(1..5)
     self.name = COMPUTER_SKILL_ASSG.keys[i]
     self.bhvr = COMPUTER_SKILL_ASSG.values[i]
   end
 
-  def choose
-    self.move = Move.new(Move::VALUES.keys.sample)
+  def find_dominant_move(history)
+    return Move::VALUES.keys.sample if history == []
+    freq = {}
+    history.uniq.each do |h|
+      freq[h] = history.count(h)
+    end
+    history.max_by { |v| freq[v] }
+  end
+
+  def counter_move(history)
+    # finds the two counter moves for the given move
+    moves_to_pick = []
+    dominant_move = find_dominant_move(history)
+    Move::VALUES.keys.each do |k|
+      moves_to_pick << k if Move.new(k) > Move.new(dominant_move)
+    end
+    moves_to_pick << Move::VALUES.keys.sample
+  end
+
+  def behaviour_1(history)
+    # Creates an array from two counter moves + n number of random moves
+    counter_move(history).sample
+  end
+
+  def behaviour_2
+    Move::VALUES.keys.sample
+  end
+
+  def behaviour_3
+    :rock
+  end
+
+  def behaviour_4
+    [:rock, :paper, :scissors].sample
+  end
+
+  def choose(history)
+    # choose behavior of the computer according the name
+    case bhvr
+    when 1
+      self.move = Move.new(behaviour_1(history))
+    when 2
+      self.move = Move.new(behaviour_2)
+    when 3
+      self.move = Move.new(behaviour_3)
+    when 4
+      self.move = Move.new(behaviour_4)
+    end
   end
 end
 
@@ -196,7 +244,7 @@ class RPSGame
     @human_hist = []
     @computer_hist = []
   end
-  
+
   def new_screen(level)
     puts "New #{level} starts in 2..."
     sleep(2)
@@ -216,7 +264,7 @@ class RPSGame
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}."
   end
-  
+
   def select_winner
     if human.move > computer.move
       'player'
@@ -238,7 +286,7 @@ class RPSGame
       puts "It's a tie"
     end
   end
-  
+
   def modify_score
     winner = select_winner
     case winner
@@ -248,7 +296,7 @@ class RPSGame
       computer.update_score
     end
   end
-  
+
   def modify_history(player)
     case player
     when human
@@ -257,7 +305,7 @@ class RPSGame
       computer_hist << computer.move
     end
   end
-  
+
   # TEST
   def display_history(player)
     puts "#{player.name}'s previous moves are:"
@@ -268,26 +316,23 @@ class RPSGame
       puts computer_hist
     end
   end
-  
+
   def show_score
     puts "Current score is:"
     puts "#{human.name}: #{human.score}"
     puts "#{computer.name}: #{computer.score}"
     puts ""
   end
-  
+
   def grand_winner?
     human.score == @target_rounds || computer.score == @target_rounds
   end
-  
+
   def grand_winner
     if human.score == @target_rounds
-      return human.name
+      human.name
     elsif computer.score == @target_rounds
-      return computer.name
-    else
-      #GUARD LINE
-      puts "ERROR:Leak in grand_winner condition!"
+      computer.name
     end
   end
 
@@ -302,12 +347,12 @@ class RPSGame
     return false if answer.downcase == 'n'
     return true if answer.downcase == 'y'
   end
-  
+
   def reset_score
     human.reset_score
     computer.reset_score
   end
-  
+
   # MAYBE OPTIONAL??
   def reset_hist
     @human_hist.clear
@@ -318,7 +363,7 @@ class RPSGame
     loop do
       human.choose
       modify_history(human)
-      computer.choose
+      computer.choose(@human_hist)
       modify_history(computer)
       display_moves
       display_winner
@@ -329,7 +374,7 @@ class RPSGame
       show_score
     end
   end
-  
+
   def display_grand_winner
     champion = grand_winner
     puts ""
@@ -343,7 +388,7 @@ class RPSGame
       game_loop
       display_grand_winner
       # OPTIONAL TEST LINE BELOW
-      #display_history(human)
+      # display_history(human)
       break unless play_again?
       reset_score
       reset_hist
@@ -353,4 +398,4 @@ class RPSGame
   end
 end
 
-RPSGame.new(3).play
+RPSGame.new(2).play
