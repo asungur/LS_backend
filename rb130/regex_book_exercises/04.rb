914b68fd48fe19f9d8f5489142efabc063c4d996
asungur <sunguralican@gmail.com>
2019-10-17 18:30:33 +0000

    regex exercises

---
+++ rb130/regex_book_exercises/04.rb
@@ -0,0 +1,8 @@
+# Write a method that changes every arithmetic operator (+, -, *, /) to a '?' and returns the resulting string. Don't modify the original string.
+
+def mysterious_math(string)
+  string.gsub(/[\+\-*\/]/, '?')
+end
+
+p mysterious_math('4 + 3 - 5 = 2')           # -> '4 ? 3 ? 5 = 2'
+p mysterious_math('(4 * 3 + 2) / 7 - 1 = 1') # -> '(4 ? 3 ? 2) ? 7 ? 1 = 1'
