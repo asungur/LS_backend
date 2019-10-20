442d9e1fc118b85be1a3e2dfeae0fd0181b0ef4b
asungur <sunguralican@gmail.com>
2019-10-20 15:01:18 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
+++ exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
@@ -1,3 +1,23 @@
-text = "asda    2a"
+class Palindromes
+  attr_accessor :palindromes
 
-puts "has tab" if text.match(/\t/)
+  def initialize(:max_factor, min_factor: 1)
+    @min_factor = :min_factor
+    @max_factor = :max_factor
+  end
+
+  def generate
+    @palindromes = (min_factor..max_factor)
+      .to_a
+      .repeated_combination(2)
+      .each_with_object({}) do |(num1, num2), result|
+      if palindrome? num1 * num2
+        result[num1 * num2] ||= []
+        result[num1 * num2] << [num1, num2]
+      end
+    end.sort
+  end
+
+
+
+end
