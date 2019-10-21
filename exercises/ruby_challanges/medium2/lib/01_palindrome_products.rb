d43e3f5089395ffbdfb6b7e40df4207ab1d8ffc1
asungur <sunguralican@gmail.com>
2019-10-21 11:03:55 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
+++ exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
@@ -1,22 +1,31 @@
 class Palindromes
-  attr_accessor :palindromes
   attr_reader :max_factor, :min_factor
 
   def initialize(input)
     @min_factor = input[:min_factor].nil? ? 1 : input[:min_factor]
     @max_factor = input[:max_factor]
-    @palindromes = []
+    @palindromes = Hash.new { |hash, key| hash[key] = [] }
   end
 
   def generate
-    (@min_factor..@max_factor).to_a.each do |num|
-      if num.to_s.size == 1 || num.to_s == num.to_s.reverse
-        @palindromes << num 
-      end
+    (@min_factor..@max_factor).to_a.repeated_combination(2) do |num1, num2|
+      @palindromes[num1 * num2] << [num1,num2] if mirrored?(num1 * num2)
     end
     @palindromes
   end
-  
+
+  def mirrored?(num)
+    num.to_s == num.to_s.reverse
+  end
+
+  def largest
+    max_palindrome = @palindromes.max_by { |k, v| k }
+    Struct.new(:value, :factors).new(max_palindrome[0], max_palindrome[1])
+  end
+
+  def smallest
+    min_palindrome = @palindromes.min_by { |k, v| k }
+    Struct.new(:value, :factors).new(min_palindrome[0], min_palindrome[1])
+  end
 end
 
-p Palindromes.new(max_factor: 99, min_factor: 10).generate
