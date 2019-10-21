0fe93f1a86797434912650e6e620655c9b40f228
asungur <sunguralican@gmail.com>
2019-10-21 09:19:34 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
+++ exercises/ruby_challanges/medium2/lib/01_palindrome_products.rb
@@ -1,23 +1,22 @@
 class Palindromes
   attr_accessor :palindromes
+  attr_reader :max_factor, :min_factor
 
-  def initialize(:max_factor, min_factor: 1)
-    @min_factor = :min_factor
-    @max_factor = :max_factor
+  def initialize(input)
+    @min_factor = input[:min_factor].nil? ? 1 : input[:min_factor]
+    @max_factor = input[:max_factor]
+    @palindromes = []
   end
 
   def generate
-    @palindromes = (min_factor..max_factor)
-      .to_a
-      .repeated_combination(2)
-      .each_with_object({}) do |(num1, num2), result|
-      if palindrome? num1 * num2
-        result[num1 * num2] ||= []
-        result[num1 * num2] << [num1, num2]
+    (@min_factor..@max_factor).to_a.each do |num|
+      if num.to_s.size == 1 || num.to_s == num.to_s.reverse
+        @palindromes << num 
       end
-    end.sort
+    end
+    @palindromes
   end
-
-
-
+  
 end
+
+p Palindromes.new(max_factor: 99, min_factor: 10).generate
