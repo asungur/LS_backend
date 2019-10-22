7d2f0402c18c900b509949d8dbd5b80acf549b25
asungur <sunguralican@gmail.com>
2019-10-22 18:40:06 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/02_wordy.rb
+++ exercises/ruby_challanges/medium2/lib/02_wordy.rb
@@ -1,5 +1,10 @@
 class WordProblem
-  attr_reader :operation
+  OPERATIONS = {
+  '+' => 'plus',
+  '-' => 'minus',
+  '/' => 'divided by',
+  '*' => 'multiplied by',
+  }
 
   def initialize(question)
     unless question =~ /What is /
@@ -8,10 +13,31 @@ class WordProblem
       @operation = question.gsub(/(What is |\?)/, '')
     end
   end
-  
-    
-  
+
+  def answer
+    conversion
+    calculation
+  end
+
+  private
+
+  def conversion
+    OPERATIONS.each do |k,v|
+      @operation.gsub!(v, k)
+    end
+  end
+
+  def calculation
+    operation_arr = @operation.split(' ')
+    num1 = operation_arr.shift.to_i
+    while operation_arr.size > 0 do
+      operation = operation_arr.shift
+      unless OPERATIONS.keys.include? operation
+        raise ArgumentError, "Invalid Operator"
+      end
+      num2 = operation_arr.shift.to_i
+      num1 = num1.send(operation, num2)
+    end
+    num1
+  end
 end
-
-
-p WordProblem.new('What is 3 divided by 3?').operation
