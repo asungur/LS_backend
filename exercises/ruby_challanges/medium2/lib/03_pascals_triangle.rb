8eee66452a26fb2075417f4b747d40e83ae9fce1
asungur <sunguralican@gmail.com>
2019-10-27 14:19:57 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/03_pascals_triangle.rb
+++ exercises/ruby_challanges/medium2/lib/03_pascals_triangle.rb
@@ -6,15 +6,20 @@ class Triangle
   end
 
   private
-  
+
   def generate
     @rows = []
-    last_row = [1]
-    1.upto(@max) do |num|
-      @rows << last_row if num == 1
-      temp = []
-      num.times { |n| last_row[n] }
+    1.upto(@max) do |row_num|
+      row_arr = []
+      1.upto(row_num) do |num|
+        if num == 1 || num == row_num
+          row_arr << 1
+        else
+          new_val = @rows[row_num - 2][num - 2] + @rows[row_num - 2][num - 1]
+          row_arr << new_val
+        end
+      end
+      @rows << row_arr
     end
   end
 end
-
