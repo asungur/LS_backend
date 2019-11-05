1a7a80c26cfd7257ab36479dd85d6bffe27f68bc
asungur <sunguralican@gmail.com>
2019-11-05 20:31:44 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/06_robot_name.rb
+++ exercises/ruby_challanges/medium2/lib/06_robot_name.rb
@@ -1,15 +1,29 @@
 class Robot
+  @@robot_names = []
   attr_reader :name
   def initialize
-    @name = generate_name
+    generate_name
   end
 
   def generate_name
-    
+    random_name = nil
+    alphabetical = ('A'..'Z').to_a
+    loop do
+      first_char = alphabetical[rand(alphabetical.size - 1)]
+      second_char = alphabetical[rand(alphabetical.size - 1)]
+      third_char = rand(10)
+      fourth_char = rand(10)
+      fifth_char = rand(10)
+      chars_arr = [first_char, second_char, third_char, fourth_char, fifth_char]
+      random_name = chars_arr.map(&:to_s).join('')
+      break unless @@robot_names.include?(random_name)
+    end
+    @@robot_names << random_name
+    @name = random_name
   end
 
   def reset
-    
+    @@robot_names.delete(@name)
+    generate_name
   end
 end
-
