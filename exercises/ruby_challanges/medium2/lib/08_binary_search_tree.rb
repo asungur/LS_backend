fbd77b85ae5daa6203e02745f03cbff722165be1
asungur <sunguralican@gmail.com>
2019-11-07 20:30:47 +0000

    ruby challanges

--- exercises/ruby_challanges/medium2/lib/08_binary_search_tree.rb
+++ exercises/ruby_challanges/medium2/lib/08_binary_search_tree.rb
@@ -1,7 +1,23 @@
 class Bst
-  @tree = []
-  def initialize(data)
-    @tree << data + [] + []
+  attr_reader :data, :left, :right
+  def initialize(int)
+    @data = int
   end
-  
+
+  def insert(int)
+    case int <=> data
+    when 0, -1
+      @left&.insert(int) || @left = Bst.new(int)
+    else
+      @right&.insert(int) || @right = Bst.new(int)
+    end
+  end
+
+  def each
+    return to_enum(:each) unless block_given?
+
+    @left.each { |node| yield(node) } if @left
+    yield(@data)
+    @right.each { |node| yield(node) } if @right
+  end
 end
