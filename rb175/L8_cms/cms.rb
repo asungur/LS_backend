6ae3f8711359ebec81361daf2ebbaec862991084
asungur <sunguralican@gmail.com>
2019-12-06 18:06:04 +0000

    tests and updated features

--- rb175/L8_cms/cms.rb
+++ rb175/L8_cms/cms.rb
@@ -1,7 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
+require "redcarpet"
 
 root = File.expand_path("..",__FILE__)
+
+configure do
+  enable :sessions
+  set :session_secret, 'super secret'
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
 
 get "/" do
   @files = Dir["./data/*"].map { |path| File.basename(path) }
@@ -10,7 +32,11 @@ end
 
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
-  
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+
+  if File.file?(file_path)
+    load_file_content(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
