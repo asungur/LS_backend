195c07d9931d2899122148a3ce00cefe29eff1a9
asungur <sunguralican@gmail.com>
2019-12-06 16:24:24 +0000

    plain text

--- rb175/L8_cms/cms.rb
+++ rb175/L8_cms/cms.rb
@@ -1,7 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
 
+root = File.expand_path("..",__FILE__)
+
 get "/" do
   @files = Dir["./data/*"].map { |path| File.basename(path) }
   erb :index
+end
+
+get "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+  
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
 end
