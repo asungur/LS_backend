952dcda8dd5f3c5476f6a24965a90a1ec14a4ab4
asungur <sunguralican@gmail.com>
2019-12-17 11:28:39 +0000

    cms

--- rb175/L8_cms/cms.rb
+++ rb175/L8_cms/cms.rb
@@ -21,22 +21,72 @@ def load_file_content(path)
     headers["Content-Type"] = "text/plain"
     content
   when ".md"
-    render_markdown(content)
+    erb render_markdown(content)
+  end
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
   end
 end
 
 get "/" do
-  @files = Dir["./data/*"].map { |path| File.basename(path) }
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
   erb :index
 end
 
-get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+get "/new" do
+  erb :new
+end
 
-  if File.file?(file_path)
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  if File.exist?(file_path)
     load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
+end
+
+post "/create" do
+  filename = params[:filename].to_s
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{params[:filename]} has been created."
+
     redirect "/"
   end
 end
--- rb175/L8_cms/test/cms_test.rb
+++ rb175/L8_cms/test/cms_test.rb
@@ -2,58 +2,121 @@ ENV["RACK_ENV"] = "test"
 
 require "minitest/autorun"
 require "rack/test"
+require "fileutils"
 
 require_relative "../cms"
 
-class AppTest < Minitest::Test
+class CMSTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
 
+  def setup
+    FileUtils.mkdir_p(data_path)
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def test_index
+
+    create_document "about.md"
+    create_document "changes.txt"
+
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
   end
 
   def test_viewing_text_document
+
+    create_document "history.txt", "Ruby 0.95 released"
+
     get "/history.txt"
     
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "Ruby 0.95 released"
-    assert_includes last_response.body, "Ruby 1.0 released"
-    assert_includes last_response.body, "2000 - Ruby 1.6 released"
-    assert_includes last_response.body, "2013 - Ruby 2.0 released."
-    assert_includes last_response.body, "Yukihiro Matsumoto"
+  end
+
+   def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
   def test_document_not_found
     get "/notafile.ext"
-    
-    assert_equal 302, last_response.status # Assert that the user was redirected
-    
-    get last_response["Location"] # Request the page that the user was redirected to
-    
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
     assert_equal 200, last_response.status
     assert_includes last_response.body, "notafile.ext does not exist"
-    
-    get "/" # Reload the page
-    refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
   end
 
-  def test_index
-    get "/"
+  def test_editing_document
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
 
     assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.md"
-    assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_includes last_response.body, "changes.txt has been updated"
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
+  end
+
+  def test_view_new_document_form
+    get "/new"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_create_new_document
+    post "/create", filename: "test.txt"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_includes last_response.body, "test.txt has been created"
+
+    get "/"
+    assert_includes last_response.body, "test.txt"
+  end
+
+  def test_create_new_document_without_filename
+    post "/create", filename: ""
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "A name is required"
   end
 end
